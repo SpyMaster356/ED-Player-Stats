3909bc4f45bd1dda990c26ef210ffc2c04de8b80
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-04-12 15:14:39 -0400

    add legend to pie chart

--- app/scripts/directives/pie-chart.js
+++ app/scripts/directives/pie-chart.js
@@ -29,20 +29,26 @@
 
       var render = function (data) {
         var
-          width = 500,
-          height = 500,
+          width = 800,
+          height = 400,
           radius = 200,
-          color = d3.scale.category20();
+          color = d3.scale.category20(),
+          legendRectSize = 18,
+          legendSpacing = 4;
 
         svg.selectAll('*').remove();
 
         if(!data) { return; }
 
+        var total = d3.sum(data, function (d) {return d.value;});
+
+        var toPercent = d3.format('0.1%');
+
         var chart = svg
           .attr('width', width)
           .attr('height', height)
           .append('g')
-            .attr('transform', 'translate(200,250)');
+            .attr('transform', 'translate(200,200)');
 
         var arc = d3.svg.arc()
           .innerRadius(radius/4 * 3)
@@ -58,6 +64,35 @@
             .append('path')
               .attr('fill', function(d, i) { return color(i); } )
               .attr('d', arc);
+
+        var legend = svg.selectAll('.legend')
+          .data(data)
+            .enter()
+              .append('g')
+                .attr('class', 'legend')
+                .attr('transform', function(d, i) {
+                  var height = legendRectSize + legendSpacing;
+                  var vert = i * height;
+
+                  return 'translate(' + 450 + ',' + vert + ')';
+                });
+
+        legend.append('text')
+          .attr('x', -5)
+          .attr('y', legendRectSize - legendSpacing)
+          .attr('text-anchor', 'end')
+          .text(function(d) { return toPercent(d.value / total); });
+
+        legend.append('rect')
+          .attr('width', legendRectSize)
+          .attr('height', legendRectSize)
+          .attr('fill', function(d, i) { return color(i); } )
+          .style('stroke', function(d, i) { return color(i); } );
+
+        legend.append('text')
+          .attr('x', legendRectSize + legendSpacing)
+          .attr('y', legendRectSize - legendSpacing)
+          .text(function(d) { return d.label; });
       };
 
       init();
